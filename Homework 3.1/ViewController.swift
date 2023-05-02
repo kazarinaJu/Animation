f621e008905f141f8a9f5718a0ed26f4d19c6366
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-05-02 15:44:13 +0500

    Initial Commit

---
+++ Homework 3.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 3.1
+//
+//  Created by Юлия Ястребова on 02.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
