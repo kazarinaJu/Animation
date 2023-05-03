99eb9282e6d7698678b042c488f0e7b5ea818896
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-05-03 16:59:31 +0500

    ass datastore

--- Homework 3.1/Services/DataStore.swift
+++ Homework 3.1/Services/DataStore.swift
@@ -5,4 +5,70 @@
 //  Created by Юлия Ястребова on 03.05.2023.
 //
 
-import Foundation
+final class DataStore {
+    static let shared = DataStore()
+    
+    let preset = [
+        "pop",
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "squeezeLeft",
+        "squeezeRight",
+        "squeezeDown",
+        "squeezeUp",
+        "fadeIn",
+        "fadeOut",
+        "fadeOutIn",
+        "fadeInLeft",
+        "fadeInRight",
+        "fadeInDown",
+        "fadeInUp",
+        "zoomIn",
+        "zoomOut",
+        "fall",
+        "shake",
+        "flipX",
+        "flipY",
+        "morph",
+        "squeeze",
+        "flash",
+        "wobble",
+        "swing"
+    ]
+    
+    let curve = [
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeOutQuad",
+        "easeInOutQuad",
+        "easeInCubic",
+        "easeOutCubic",
+        "easeInOutCubic",
+        "easeInQuart",
+        "easeOutQuart",
+        "easeInOutQuart",
+        "easeInQuint",
+        "easeOutQuint",
+        "easeInOutQuint",
+        "easeInExpo",
+        "easeOutExpo",
+        "easeInOutExpo",
+        "easeInCirc",
+        "easeOutCirc",
+        "easeInOutCirc",
+        "easeInBack",
+        "easeOutBack",
+        "easeInOutBack"
+    ]
+    
+    private init() {}
+}
