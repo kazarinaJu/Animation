0d72d02cfe17aae7e3af647f9c022934f1df93c8
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-05-03 13:18:16 +0500

    set project

---
+++ Homework 3.1/Models/Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  Homework 3.1
+//
+//  Created by Юлия Ястребова on 03.05.2023.
+//
+
+import Foundation
---
+++ Homework 3.1/Services/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  Homework 3.1
+//
+//  Created by Юлия Ястребова on 03.05.2023.
+//
+
+import Foundation
